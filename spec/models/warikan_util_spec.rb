6ea785a9da1468c86329739ec7a16901e4cc5d70
rasenn <i.know.defeat@gmail.com>
2016-03-24 00:27:13 +0900

    create controller, view

--- spec/models/warikan_util_spec.rb
+++ spec/models/warikan_util_spec.rb
@@ -3,15 +3,18 @@ require 'rails_helper'
 RSpec.describe WarikanUtil, :type => :model do
   describe "#create_list" do
     it "can be create" do
+      expect(WarikanUtil.create_list("list name").integer?).to eq true
       expect(WarikanUtil.create_list.integer?).to eq true
     end
 
     it "create different id every time" do
-      one = WarikanUtil.create_list
-      two = WarikanUtil.create_list
+      one = WarikanUtil.create_list("same name")
+      two = WarikanUtil.create_list("same name")
       expect(one != two).to eq true
+      three = WarikanUtil.create_list
+      four = WarikanUtil.create_list
+      expect(three != four).to eq true
     end
-    
   end
 
   describe "#add_member" do
@@ -37,6 +40,7 @@ RSpec.describe WarikanUtil, :type => :model do
       end
       it "can add kingaku" do
         expect(WarikanUtil.add_kingaku(@list_id, @member_id, 123, "warikan memo").instance_of?(Fixnum)).to eq true
+        expect(Kingaku.all.count > 0 ).to eq true
       end
     end
     
@@ -53,4 +57,15 @@ RSpec.describe WarikanUtil, :type => :model do
       end
     end
   end
+
+  describe "#already_added?" do
+    it "when make same kingaku it expect true" do
+      cost = 100
+      list_id = WarikanUtil.create_list("name")
+      member_id = WarikanUtil.add_member(list_id,"name")
+      WarikanUtil.add_kingaku(list_id,member_id,cost,"memo")
+      expect(WarikanUtil.already_added?(list_id,cost)).to eq true
+    end
+  end
+  
 end
